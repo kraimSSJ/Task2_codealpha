37919cece2609b09fdd7d961e8914a758c5faf27
Ayoub Kraim <kraim.ayoub2020@gmail.com>
2026-03-08 23:22:38 +0000

    fix: use environment variables for supabase connection

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://rzioejdhvukqmamdmeqd.supabase.co'
-const supabaseKey = 'PASTE_YOUR_ANON_PUBLIC_KEY_HERE'
+// This tells the app to look at the "Environment Variables" we set in Vercel
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
